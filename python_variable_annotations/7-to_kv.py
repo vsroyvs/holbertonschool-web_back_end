0dffc22fec49ee74dd18388d986fbb659e079670
Roy <vargas.sap321@gmail.com>
2023-09-28 11:54:35 -0500

    fix: pycodestyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k : str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """return tuple"""
     return k, pow(v, 2)
