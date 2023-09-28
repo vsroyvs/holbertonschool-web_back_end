93e158fcab9d3ccb779b0dfaa57f794f5ea97b4c
Roy <vargas.sap321@gmail.com>
2023-09-28 10:32:10 -0500

    feat: function that return a sum of list element

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module to sum a element of list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Sum a list of floats"""
+    result = sum(x for x in input_list)
+    return result
