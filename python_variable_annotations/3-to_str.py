59d28f30cc2c7f891110ab261ca439e392471b1b
Roy <vargas.sap321@gmail.com>
2023-09-28 10:09:19 -0500

    feat: function that return the string representation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that implements a function
+that return a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """return a floor of a float"""
+    return str(n)
