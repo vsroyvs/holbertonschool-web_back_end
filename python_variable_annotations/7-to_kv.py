be0aa7d4db25b348d893dcc894bec20d3add8fd3
Roy <vargas.sap321@gmail.com>
2023-09-28 11:51:11 -0500

    feat: function that returns a tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module to return a tuple"""
+from typing import Tuple, Union
+
+
+def to_kv(k : str, v: Union[int, float]) -> Tuple[str, float]:
+    """return tuple"""
+    return k, pow(v, 2)
