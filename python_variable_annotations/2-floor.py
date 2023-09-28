9123cea572befce038cf33d6b5b3d1bf842e6583
Roy <vargas.sap321@gmail.com>
2023-09-28 10:03:06 -0500

    feat: function that return the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that implements a function
+that return a floor of a float"""
+
+
+def floor(a: float) -> int:
+    """return a floor of a float"""
+    return int(a)
