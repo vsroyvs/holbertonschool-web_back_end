126c738e8f0d793e1a55b16e27b59b4e2432ce7c
Roy <vargas.sap321@gmail.com>
2023-09-28 12:22:04 -0500

    feat: function to create a function and multiplier by parameter

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Module to return a multiplier by multiplier function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """create a multiplier function"""
+    def multiplier_function(x: float) -> float:
+        """return the multiplier function by multiplier"""
+        return multiplier * x
+    
+    return multiplier_function
