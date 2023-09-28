861fb5493245b0621a98bce778db39fc3b60187d
Roy <vargas.sap321@gmail.com>
2023-09-28 09:52:21 -0500

    feat: feat: implement a function that returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that implements a function
+that return a concatenated string of two strings"""
+
+
+def concat(a: str, b: str) -> str:
+    """return a concatenated string of two strings"""
+    return a + b
