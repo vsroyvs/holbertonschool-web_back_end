193ec301ff66bc2078221b5c76311e4d7e7d2312
Roy <vargas.sap321@gmail.com>
2023-10-01 00:35:37 -0500

    feat: Async function that return a random delay time

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Asynchronous coroutine """
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int= 10) -> float:
+    """ Returns a random delay time"""
+    number_random = random.randint(0, max_delay)
+    await asyncio.sleep(number_random)
+    return number_random
