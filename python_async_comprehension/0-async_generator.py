ffaa01c60e098365af735e18606e7458a05b3573
Roy <vargas.sap321@gmail.com>
2023-10-01 23:29:09 -0500

    feat: Async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Asynck generator"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator(float, None, None):
+    """Asynck generator"""
+    for _ in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
