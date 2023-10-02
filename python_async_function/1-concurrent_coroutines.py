c8e9ff82214323719564da6a67db5b192b243a8e
Roy <vargas.sap321@gmail.com>
2023-10-01 21:09:47 -0500

    fix: correct in return a correct number of floats

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ List of floats, length n times"""
-    list = [wait_random(max_delay) for _ in n]
+    list = [wait_random(max_delay) for _ in range(n)]
     delay_times = [await i for i in asyncio.as_completed(list)]
     return sorted(delay_times)
