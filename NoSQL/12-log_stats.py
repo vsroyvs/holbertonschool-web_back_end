abdbbc6bce56f4203d31eba50d4f269834f17594
Roy <vargas.sap321@gmail.com>
2023-10-04 00:26:13 -0500

    script that provides some stats about Nginx logs stored in MongoDB

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 
 if __name__ == '__main__':
-    myclient = MongoClient("mongodb://localhost:27017/")
+    myclient = MongoClient("mongodb://localhost:27017")
     mydb = myclient.logs
     mycol = mydb.nginx
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
