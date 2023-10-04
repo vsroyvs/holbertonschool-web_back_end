f3e8efe6937542c1c1d2cb4b006ee9c32e9234c6
Roy <vargas.sap321@gmail.com>
2023-10-03 22:44:03 -0700

    fix: give a executable permission

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,13 +5,13 @@ from pymongo import MongoClient
 
 if __name__ == '__main__':
     myclient = MongoClient("mongodb://localhost:27017")
-    mydb = myclient.logs
-    mycol = mydb.nginx
+    mydb = myclient['logs']
+    mycol = mydb['nginx']
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     x = mycol.count_documents({})
 
     print(f"{x} logs")
     print("Methods:")
     for i in method:
-        print(f"method {i}: {mycol.count_documents({'method': i})}")
+        print(f"\tmethod {i}: {mycol.count_documents({'method': i})}")
     print(f"{mycol.count_documents({'method': 'GET', 'path': '/status'})} status check")
