d3fb56e3c2808c1246239a54a85f1288856e4315
Roy <vargas.sap321@gmail.com>
2023-10-03 23:59:26 -0500

    rollback

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python3
-"""module that that provides some stats about Nginx logs stored in MongoDB"""
-from pymongo import MongoClient
+"""module that returns the list documents having a specific topic"""
 
-
-if __name__ == '__main__':
-    myclient = MongoClient("mongodb://localhost:27017/")
-    mydb = myclient['logs']
-    mycol = mydb['nginx']
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    x = mycol.find().count()
-
-    print(f"{x} logs")
-    print("Methods:")
-    for i in method:
-        print(f"method {i}: {mycol.find({'method': i}).count()}")
-    print(f"{mycol.find({'method': 'GET', 'path': '/status'}).count()} status check")
+def schools_by_topic(mongo_collection, topic):
+    """documents list"""
+    document_finded = mongo_collection.find({'topic': topic})
+    document_finded = mongo_collection.find({'topics': topic})
+    return document_finded
