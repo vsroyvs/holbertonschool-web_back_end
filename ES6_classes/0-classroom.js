b9ca282ba1c74be812c03fe83143f57eb3f96ca7
Roy <vargas.sap321@gmail.com>
2023-10-16 12:11:04 -0500

    feat: Implement a class named ClassRoom

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+    constructor(maxStudentsSize){
+        if (this.constructor !== ClassRoom && this.evacuationWarningMessage === undefined) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        if (typeof maxStudentsSize !== 'number') throw TypeError('maxStudentsSize must be a number');
+        this._maxStudentsSize = maxStudentsSize
+    } 
+};
