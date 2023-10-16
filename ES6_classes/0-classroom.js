f12aa0f55c636028360b111fbda24f465e42b24e
Roy <vargas.sap321@gmail.com>
2023-10-16 17:11:46 -0500

    fix: eslint

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -2,5 +2,5 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     if (typeof maxStudentsSize !== 'number') throw TypeError('maxStudentsSize must be a number');
     this._maxStudentsSize = maxStudentsSize;
-  } 
+  }
 }
