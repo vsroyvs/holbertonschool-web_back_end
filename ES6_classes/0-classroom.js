ded5b0f29b260c21be799a5d145d736d46acf4ea
Roy <vargas.sap321@gmail.com>
2023-10-16 17:08:43 -0500

    fix: typo

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,8 +1,5 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize){
-        if (this.constructor !== ClassRoom && this.evacuationWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+    constructor(maxStudentsSize) {
         if (typeof maxStudentsSize !== 'number') throw TypeError('maxStudentsSize must be a number');
         this._maxStudentsSize = maxStudentsSize;
     } 
