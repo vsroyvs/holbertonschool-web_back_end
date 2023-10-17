3d933d7f57160fb53fd9093965cf4fcd7e93214d
Roy <vargas.sap321@gmail.com>
2023-10-16 22:53:04 -0500

    fix: Passes lint correction

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
 import ClassRoom from "./0-classroom.js";
 
-function initializeRooms(){
-    const room = new ClassRoom(19);
-    const room1 = new ClassRoom(20);
-    const room2 = new ClassRoom(34);
-    return [room, room1, room2];
+export default function initializeRooms() {
+  const room = new ClassRoom(19);
+  const room1 = new ClassRoom(20);
+  const room2 = new ClassRoom(34);
+  return [room, room1, room2];
 }
