841bc88616977bc33e13c9000deacdc406939596
Roy <vargas.sap321@gmail.com>
2023-10-17 09:43:49 -0500

    feat: implement a inheritaced class and set a method

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -18,6 +18,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the NUMBER_OF_FLOORS floors.`;
+    return `Evacuate slowly the ${this._floors} floors.`;
   }
 }
