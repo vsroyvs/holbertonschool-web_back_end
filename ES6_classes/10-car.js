5e47e93d1990231a23354b42f1ad5da26ac4250b
Roy <vargas.sap321@gmail.com>
2023-10-17 11:47:18 -0500

    feat: implement a class with a method that return anew object of the class - as a clone

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,11 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof color !== 'string') throw TypeError('color must be a string');
-    if (typeof brand !== 'string') throw TypeError('brand must be a string');
-    if (typeof motor !== 'string') throw TypeError('motor must be a string');
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
