50d02b2045d741c33c15230636fd3a9f6fcadb9e
Roy <vargas.sap321@gmail.com>
2023-10-16 19:58:46 -0500

    feat: ibject instance

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom.js";
+
+function initializeRooms(){
+    const room = new ClassRoom(19);
+    const room1 = new ClassRoom(20);
+    const room2 = new ClassRoom(34);
+    return [room, room1, room2];
+}
