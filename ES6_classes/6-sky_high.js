448335a68be45b1289f00876417438e24d865d6c
Roy <vargas.sap321@gmail.com>
2023-10-17 09:37:25 -0500

    feat: implement a inheritaced class and set a method

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') throw TypeError('floors must be a number');
+    this._floors = floors;
+  }
+
+  // getter and setter
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') throw TypeError('value must be a number');
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the NUMBER_OF_FLOORS floors.`;
+  }
+}
