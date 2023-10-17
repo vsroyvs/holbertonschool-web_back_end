96cc01731d28b8ed43e682744c46c4c88fc1db0a
Roy <vargas.sap321@gmail.com>
2023-10-17 00:33:00 -0500

    feat: Implement a class, with getters and setters, and public and static methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') throw TypeError('amount must be a number');
+    if (!(currency instanceof Currency)) throw TypeError('currency must be a Currency instane');
+    this._amount = amount;
+    this._currency = currency;
+}
+
+  // getter and setter
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') throw TypeError('amount must be a number');
+    this._amount = value;
+}
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) throw TypeError('currency must be a Currency instane');
+    this._currency = value
+  }
+
+  // methods
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._name})`
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
