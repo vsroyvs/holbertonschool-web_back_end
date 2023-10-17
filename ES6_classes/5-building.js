27efc894650ee5700445ce6dde36585e0e43fad5
Roy <vargas.sap321@gmail.com>
2023-10-17 01:07:00 -0500

    Implement an abstract class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if(typeof sqft !== 'number') throw TypeError('sqft must be a number');
+    if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
     this._sqft = sqft;
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('Abstract Class can not be instantiated.');
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-}
+  }
 
   // getter and setter
   get sqft() {
@@ -13,7 +13,7 @@ export default class Building {
   }
 
   set sqft(value) {
-    if(typeof value !== 'number') throw TypeError('sqft must be a number');
+    if (typeof value !== 'number') throw TypeError('sqft must be a number');
     this._sqft = value;
   }
 }
