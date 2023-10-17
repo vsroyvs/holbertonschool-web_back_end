fed57774e7e4f4769322942ade3a17aaa0a08769
Roy <vargas.sap321@gmail.com>
2023-10-17 10:16:49 -0500

    feat: implement a class with toSting method to define a default string

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw TypeError('name must be a string');
+    if (typeof code !== 'string') throw TypeError('code must be a string');
+    this._name = name;
+    this._code = code;
+  }
+
+  // getter ande setter
+  get name() {
+    return this._name;
+}
+
+  set name(value) {
+    if (typeof value !== 'string') throw TypeError('value must be a string');
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+  set code(value) {
+    if (typeof value !== 'string') throw TypeError('value must be a string');
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
