a12442d1b800b916eb93c045744589d5c927e67d
Roy <vargas.sap321@gmail.com>
2023-10-19 01:01:15 -0500

    feat: definition of deafaults value parameters

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -2,4 +2,4 @@ const getSumOfHoods = (initialNumber,
   expansion1989 = 89,
   expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
 
-  export default getSumOfHoods;
+export default getSumOfHoods;
