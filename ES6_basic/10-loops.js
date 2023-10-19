e806e297ae87a1e61e6babc1a3bb4240e2d1eeac
Roy <vargas.sap321@gmail.com>
2023-10-19 16:38:11 -0500

    feat: generate new array from parameters

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-    for (let value of array) {
+  for (let value of array) {
     value = appendString + value;
     newArray.push(value);
   }
