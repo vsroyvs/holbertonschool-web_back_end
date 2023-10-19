05ed91eb7f4855bf294ca6645d93fabca9e7d73e
Roy <vargas.sap321@gmail.com>
2023-10-19 17:48:40 -0500

    feat: implement a function that returns a objects with a list of employees and number of deparments

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,11 +1,9 @@
-import createEmployeesObject from './11-createEmployeesObject';
-
 export default function createReportObject(employeesList) {
   const object = {
     allEmployees: employeesList,
-    getNumberOfDepartments(employeesList){
+    getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
-    }, 
-  }
+    },
+  };
   return object;
 }
