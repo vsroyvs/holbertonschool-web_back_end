2ae024b6d7e3ef65ebe314bbfd614bee32c4ff4d
Roy <vargas.sap321@gmail.com>
2023-10-24 16:14:18 -0500

    feat:implement a promise function

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (true) resolve("es true");
+    else reject("es false");
+  });
+}
+
+export default getResponseFromAPI;
