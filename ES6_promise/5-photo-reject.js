5be630036fe839e5919fc6ac04a027ea74ab8916
Roy <vargas.sap321@gmail.com>
2023-10-25 13:19:10 -0500

    fix: lint missinf comma

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
   return Promise.reject(
-    new Error(`${filename} cannot be processed`)
+    new Error(`${filename} cannot be processed`),
   );
 }
