7885021e66fb8b6b7602f8d18cff53fd16690559
Roy <vargas.sap321@gmail.com>
2023-10-25 18:44:37 -0500

    feat: implement a function that return an array when all promises was settled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
