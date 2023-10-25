a75a0ea5a590d5f547c97efd898095bd06b54a63
Roy <vargas.sap321@gmail.com>
2023-10-25 01:02:02 -0500

    feat: implement function that handle multiple promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import createUser  from "./utils";
+import uploadPhoto from "./utils";
+
+function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+  .then((data) => {
+    console.log(`${data[1].body} ${data[0].firstName} ${data[0].lastName}`);
+  })
+  .catch(() => console.log('Signup system offline'));
+}
+
+export default handleProfileSignup;
