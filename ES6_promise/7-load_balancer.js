b22787e8d4c82ad0df4e205c91e0240e5a96d596
Roy <vargas.sap321@gmail.com>
2023-10-26 16:51:43 -0500

    feat: returns a promise that resolves to the value from the first settled promise

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,7 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   // Promise.race() returns a promise that resolves to the value from the first settled promise
   return Promise.race([chinaDownload, USDownload])
-    .then((data) => {
-      return data;
-    });
+    .then((data) => data);
 }
