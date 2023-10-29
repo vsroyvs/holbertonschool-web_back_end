e3fb15b95c9b8f3c62d95815f96baa38a4cec4c4
Roy <vargas.sap321@gmail.com>
2023-10-29 17:35:03 -0500

    feat: function to return an array of object

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,10 @@
 const getListStudents = () => {
   const students = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-      { id: 2, firstName: 'James', location: 'Columbia'},
-      { id: 3, firstName: 'Serena', location: 'San Francisco'},
-  ]
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    { id: 2, firstName: 'James', location: 'Columbia'},
+    { id: 3, firstName: 'Serena', location: 'San Francisco'},
+  ];
   return students;
 };
-
-console.log(getListStudents());
 
 export default getListStudents;
