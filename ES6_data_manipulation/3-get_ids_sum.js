81196da974c91e15a2e6198168380f53e8b1a2cb
Roy <vargas.sap321@gmail.com>
2023-10-29 22:06:25 -0500

    feat: function that return a sum of ids from array

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 const getStudentIdsSum = (arrayStudents) => {
   const studentIdsSum = arrayStudents.reduce(
-    (accumulator, student) => accumulator + student.id,0 );
-  return studentIdsSum
+    (accumulator, student) => accumulator + student.id, 0);
+  return studentIdsSum;
 };
 
 export default getStudentIdsSum;
